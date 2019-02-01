73566e9ca800c85f688bbabe740272a3784ec502
Kishore Nadiger <kishore.nadiger@gmail.com>
2019-02-02 00:57:30 +0530

    added new project tic tac toe

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,6 +1,7 @@
 function startGame() {
     document.turn="X";
-    setMessage(document.turn+" gets to start")
+    setMessage(document.turn+" gets to start");
+    
 }
 
 function setMessage(msg){
@@ -8,14 +9,57 @@ function setMessage(msg){
 }
 
 function nextMove(sq) {
+    if(sq.innerHTML==""){
     sq.innerHTML=document.turn;
     switchMove();
+    }else
+    {
+        setMessage("INVALID");
+    }
 }
 function switchMove(params) {
-    if (document.turn=='X') {
+    if(check(document.turn)){
+        window.alert(document.turn+"  WON THE GAME");
+        
+    }else if (document.turn=='X') {
         document.turn='O';
     } else {
         document.turn='X';
     }
     setMessage("its "+document.turn+"'s turn")
 }
+// function re(){
+//     let cell=document.querySelectorAll("td");
+//     for (let i = 0; i< cell.length; i++) {
+//         cell.textContent=' ';
+//         cell.innerHTML=' ';
+//         cell.innerText=' ';       
+//     }
+    
+//     console.log(cell);
+// }
+// function re(params) {
+//     // let cell = document.getElementsByClassName('sq');
+//     startGame();
+   
+//     console.log(cell);
+// }
+function check(move){
+    var res=false;
+    if(checkRow(1,2,3,move)||checkRow(4,5,6,move)||checkRow(7,8,9,move)||checkRow(1,4,7,move)||checkRow(2,5,8,move)||checkRow(3,6,9,move)||checkRow(1,5,9,move)||checkRow(3,5,7,move)){
+        res=true;
+    }
+    return res;
+}
+function checkRow(a,b,c,move) {
+    var result = false;
+    if(getBox(a)==move && getBox(b)==move && getBox(c)==move){
+        result=true;
+    }
+    return result;
+}
+
+function getBox(num) {
+    return document.getElementById('s'+num).innerText;
+}
+
