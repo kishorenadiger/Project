c4bc54174d6c8a11a5d6c605ca8c079087a534ff
Kishore Nadiger <kishore.nadiger@gmail.com>
2019-02-11 20:38:57 +0530

    ang app updated 2

--- angular_2/angapp/src/app/components/user/user.component.ts
+++ angular_2/angapp/src/app/components/user/user.component.ts
@@ -6,41 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  name:string ;
-  age:number;
-  email:string;
-  address:Address;
-  hobies:any[];
+  name: string ;
+  age: number;
+  email: string;
+  address: Address;
+  hobies: any[];
   constructor() { }
 
   ngOnInit() {
-    this.name='kish';
-    this.age=20;
-    this.email='kishore@getComputedStyle.c';
-    this.address={
-      street : "new bel road",
-      city:"balore",
-      state:"kar",
-    
-    }
-  this.hobies = ['vbdcfa','saddas','sadsd','wefads'];
+    this.name = 'kish';
+    this.age = 20;
+    this.email = 'kishore@getComputedStyle.c';
+    this.address = {
+      street : 'new bel road',
+      city: 'balore',
+      state: 'kar' ,
+    };
+    this.hobies = ['vbdcfa', 'saddas', 'sadsd', 'wefads'];
   }
-  addHobbies(hobby){
+  addHobbies(hobby) {
     this.hobies.unshift(hobby);
   }
-  removeHobby(hobby){
-      for(let i=0; i<this.hobies.length;i++)    
-      {
-        if (this.hobies[i]==hobby) {
-          this.hobies.splice(i,1);
+  removeHobby(hobby) {
+      for ( let i = 0; i < this.hobies.length; i++) {
+        if (this.hobies[i] === hobby) {
+          this.hobies.splice(i, 1);
         }
       }
   }
 
 }
 
-interface Address{
-  street:string;
-  city:string;
-  state:string;
+interface Address {
+  street: string;
+  city: string;
+  state: string;
 }
